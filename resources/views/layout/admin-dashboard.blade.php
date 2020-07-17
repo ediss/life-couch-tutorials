cca8dede0ca3f2ead3364f1699750fc76fa3962d
Edis Skenderi <eskenderi@egamingc.com>
2020-07-17 16:47:10 +0200

    fixing admin panel css

--- resources/views/layout/admin-dashboard.blade.php
+++ resources/views/layout/admin-dashboard.blade.php
@@ -8,11 +8,11 @@
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{ asset('AdminLTE/plugins/fontawesome-free/css/all.min.css') }}">
+  <link rel="stylesheet" href="{{ url('AdminLTE/plugins/fontawesome-free/css/all.min.css') }}">
   <!-- Theme style -->
-  <link rel="stylesheet" href="{{ asset('AdminLTE/dist/css/adminlte.min.css') }}">
+  <link rel="stylesheet" href="{{ url('AdminLTE/dist/css/adminlte.min.css') }}">
 
-  <link rel="stylesheet" href="{{ asset('AdminLTE/plugins/summernote/summernote-bs4.min.css') }}">
+  <link rel="stylesheet" href="{{ url('AdminLTE/plugins/summernote/summernote-bs4.min.css') }}">
 
   <!-- Latest compiled and minified CSS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
@@ -195,15 +195,15 @@
 <!-- ./wrapper -->
 
 <!-- jQuery -->
-<script src="{{ asset("AdminLTE/plugins/jquery/jquery.min.js" ) }}"></script>
+<script src="{{ url("AdminLTE/plugins/jquery/jquery.min.js" ) }}"></script>
 <!-- Bootstrap 4 -->
-<script src="{{ asset("AdminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js") }}"></script>
+<script src="{{ url("AdminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js") }}"></script>
 <!-- AdminLTE App -->
-<script src="{{ asset("AdminLTE/dist/js/adminlte.min.js") }}"></script>
+<script src="{{ url("AdminLTE/dist/js/adminlte.min.js") }}"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="{{ asset("AdminLTE/dist/js/demo.js") }}"></script>
+<script src="{{ url("AdminLTE/dist/js/demo.js") }}"></script>
 
-<script src="{{ asset("AdminLTE/plugins/summernote/summernote-bs4.min.js") }}"></script>
+<script src="{{ url("AdminLTE/plugins/summernote/summernote-bs4.min.js") }}"></script>
 
   <!-- Latest compiled and minified JavaScript -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js"></script>
