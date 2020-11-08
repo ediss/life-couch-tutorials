f113ed736fbb5ab80459fec5d8f5f0b40e845187
Edis Skenderi <eskenderi@egamingc.com>
2020-11-08 20:03:39 +0100

    commenting forgat pass

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -62,11 +62,11 @@
                                     </div>
                                 </div>
                             </div>
-                            @if (Route::has('password.request'))
+                            {{-- @if (Route::has('password.request'))
                             <a class="btn btn-link" href="{{ route('password.request') }}">
                                 {{ __('Forgot Your Password?') }}
                             </a>
-                            @endif
+                            @endif --}}
 
                             <div class="form-group row mb-0">
                                 <div class="col-md-8 offset-md-4">
