2d18763e389d84e967f851421f92ef2991ae1bed
edis <eskenderi@egamingc.com>
2021-09-29 16:08:57 +0200

    ..

--- resources/views/success-apply.blade.php
+++ resources/views/success-apply.blade.php
@@ -24,7 +24,7 @@ Uspešna Prijava
     <div class="row" style="margin-top:10%; margin-bottom:10%;">
         <div class="col-8 offset-2 text-justify box-shadow" style="padding:5%">
 
-
+            @dd($course)
             @if($course->is_free == 0)
                 @include('partial.succes-apply-paid-course')
             @else
